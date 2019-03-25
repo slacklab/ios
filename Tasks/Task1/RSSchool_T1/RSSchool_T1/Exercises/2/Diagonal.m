0f06ab3554252c78c191389b2ffe7bdda88e1343
Ivan Sorokoletov <feltramt@gmail.com>
2019-03-25 23:36:17 +0300

    done second: absolute difference beetwen two diagonal

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,24 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    NSInteger index = 0;
+    NSInteger primarySum = 0;
+    NSInteger secondarySum = 0;
+    
+    unsigned long count = [array count];
+    
+    for (NSString *item in array) {
+        NSArray *row = [item componentsSeparatedByCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
+        primarySum = primarySum + [[row objectAtIndex: index] integerValue];
+        secondarySum = secondarySum + [[row objectAtIndex: (count - index - 1)] integerValue];
+        index++;
+    }
+    
+    NSInteger result = labs(primarySum - secondarySum);
+    
+    return @(result);
 }
 
 @end
+
+
