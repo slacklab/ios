a5d6ff4e22448cc0888d2e718d0323bd722de850
Ivan Sorokoletov <feltramt@gmail.com>
2019-03-25 23:40:31 +0300

    done third: panagrams

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,17 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    NSString *alphabetic = @"abcdefghijklmnopqrstuvwxyz";
+    
+    for (int i = 0; i < [alphabetic length]; i++) {
+        NSString *sym = [alphabetic substringWithRange: NSMakeRange(i, 1)];
+        if (![[string lowercaseString] containsString: sym]) {
+            
+            return NO;
+        }
+    }
+    
+    return YES;
 }
 
 @end
